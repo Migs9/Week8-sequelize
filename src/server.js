e7adfe3bbe6fb3957d2e229db24218a94e9240b6
Migs9 <galleuk@gmail.com>
2023-04-18 16:11:57 +0100

    post works

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 require("dotenv").config()
 const express = require("express");
 
+const Book = require("./books/model");
+
+const bookRouter = require("./books/routes")
+
 const port = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.json());
 
+const syncTables = () => {
+    Book.sync();
+};
+
+app.use(bookRouter);
+
 app.get("/health", (req, res) => {
     res.status(200).json({message: "App works"});
 });
 
-app.get("/healthtw2", (req, res) => {
-    res.status(200).json({message: "App works YEP"});
-});
 
-app.listen(port, () => console.log("Server knows you're here"));
+app.listen(port, () => {
+    syncTables()
+    console.log("Server knows you're here")
+});
