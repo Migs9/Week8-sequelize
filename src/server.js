9ad3cca16410cc75e5951f44c3f7ceb93e068bf4
Migs9 <galleuk@gmail.com>
2023-04-18 14:41:34 +0100

    added SQL URL

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require("express");
 
-
+const port = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.json());
@@ -14,4 +14,4 @@ app.get("/healthtw2", (req, res) => {
     res.status(200).json({message: "App works YEP"});
 });
 
-app.listen(5001, () => console.log("Server knows you're here"));
+app.listen(port, () => console.log("Server knows you're here"));
