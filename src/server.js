0a2d37d3854aaad1c01cabe35d063634aa16a0ad
Migs9 <galleuk@gmail.com>
2023-04-18 14:09:58 +0100

    added nodemon dev script

--- src/server.js
+++ src/server.js
@@ -10,5 +10,8 @@ app.get("/health", (req, res) => {
     res.status(200).json({message: "App works"});
 });
 
+app.get("/healthtw2", (req, res) => {
+    res.status(200).json({message: "App works YEP"});
+});
 
 app.listen(5001, () => console.log("Server knows you're here"));
