cfdc6263cf8dbcd09dc7554d3028d9e32ec8fc9c
Migs9 <galleuk@gmail.com>
2023-04-18 13:56:05 +0100

    basic server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+const express = require("express");
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.status(200).json({message: "App works"});
+});
+
+
+app.listen(5001, () => console.log("Server knows you're here"));
