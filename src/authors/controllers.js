46aebb54f1f4a9b3b54c35001527d3469502483e
Migs9 <galleuk@gmail.com>
2023-04-23 16:50:09 +0100

    .

--- src/authors/controllers.js
+++ src/authors/controllers.js
@@ -26,39 +26,7 @@ const addAuthor = async(req, res) => {
     }
 };
 
-// - - - - - - - - - - - - - - - NOT NEEDED AT THIS POINT - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-// const updateBook = async (req, res) => {
-//     try {
-//         const updateBook = await Book.update (
-//             {author: req.body.newAuthor},
-//             {where: {title:req.body.title}},
-//         );
-//         res.status(201).json({ message:"Record Updated", updateResult: updateBook}); 
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
-
-// const deleteBook = async (req, res) => {
-//     try {
-//         const { title } = req.body; // why not move this into the where clause?
-
-//         const book = await Book.destroy ({
-//             where: {
-//                 title: title,
-//             }
-//         });
-
-//         res.status(201).json({ message:"Record Deleted", result: book}); 
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
 module.exports = {
     addAuthor,
     getAuthor,
-    // updateBook,
-    // deleteBook,
 };
--- src/authors/routes.js
+++ src/authors/routes.js
@@ -1,15 +1,10 @@
 const { Router } = require("express");
 const authorRouter = Router();
 
-const { getAuthor, addAuthor, updateBook, deleteBook } = require("./controllers");
+const { getAuthor, addAuthor } = require("./controllers");
 
 authorRouter.post("/authors/addauthor", addAuthor);
 
 authorRouter.get("/authors/getauthorandbooks/:authorname", getAuthor);
-
-// authorRouter.put("/authors/updatebook", updateBook);
-
-// authorRouter.delete("/authors/deletebook", deleteBook);
-
 
 module.exports = authorRouter;
